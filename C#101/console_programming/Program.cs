04618e5ab2e5288946baf54a151a28a584d89f25
mirzacckc <cicekcimirzacan@gmail.com>
2021-11-20 04:12:25 +0300

    İlk ders dosyalari eklendi.

---
+++ C#101/console_programming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Space!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldiniz " + name + " " + surname);
+        
+
+
+        }
+    }
+}
