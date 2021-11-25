76d7c2ae641e3f201b3bb19202bdcc99454cedab
mirzacckc <cicekcimirzacan@gmail.com>
2021-11-25 20:47:08 +0300

    Altinci ders dosyalari eklendi.

---
+++ C#101/if_else_if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >=7 && time <11 )
+                Console.WriteLine("Günaydın");
+            else if(time <= 18)
+                Console.WriteLine("İyi günler");
+            else
+                Console.WriteLine("İyi geceler");
+
+            string sonuc = time<=18 ? "İyi Günler" : "İyi Geceler";    // atanan eşitlik sağlanırsa soru işaretinden sonra ilk yazılan sağlamazsa ikinci yazılan gelecek.
+
+
+            sonuc = time >=7 && time <11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
