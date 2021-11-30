70d6454de77f0226e54fc7f7845c9b0df7f829d8
mirzacckc <cicekcimirzacan@gmail.com>
2021-11-30 17:18:45 +0300

    Dokuzuncu ders dosyalari eklendi.

---
+++ C#101/donguler_while_foreach/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //While
+           //1'den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıralım.
+           Console.Write("Lütfen sayı giriniz: ");
+           int sayi = int.Parse(Console.ReadLine());
+           int sayac = 1;
+           int toplam = 0;
+           while (sayac <= sayi)
+           {
+             toplam += sayac;
+             sayac ++; 
+           }
+
+           Console.WriteLine(toplam/sayi);
+
+           // 'a' dan 'z' ye kadar tüm harfleri console a yazdır.
+           char character = 'a';
+           while (character < 'z')
+           {
+                Console.Write(character);
+                character ++;
+           }
+
+           Console.WriteLine(" ");           
+           Console.WriteLine("***** Foreach ***** ");
+           string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+           foreach (var araba in arabalar)
+           {
+               Console.WriteLine(araba);
+           }
+
+
+        }
+    }
+}
